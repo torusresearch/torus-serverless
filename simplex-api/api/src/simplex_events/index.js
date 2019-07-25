2934c39fd91a7d0d8e523b8a7f36ad1619a4ade1
chaitanyapotti <chaitanya.potti@gmail.com>
2019-07-25 04:18:50 -0500

    modify crontab to use node-cron

--- simplex-api/api/src/simplex_events/index.js
+++ simplex-api/api/src/simplex_events/index.js
@@ -1,13 +1,18 @@
-import getEvents from './retrieveEvents'
+import getEvents from "./retrieveEvents";
 
-import createLogger from 'logging'
-const logger = createLogger('simplex_events/index.js')
+import createLogger from "logging";
+const logger = createLogger("simplex_events/index.js");
+const cron = require("node-cron");
 
-getEvents()
+cron.schedule("* * * * *", function() {
+  console.log("Starting cron job");
+  getEvents()
   .then(() => {
-    process.exit(0)
+    process.exit(0);
   })
   .catch(_error => {
-    logger.error(_error)
-    process.exit(1)
-  })
+    logger.error(_error);
+    process.exit(1);
+  });
+
+});
