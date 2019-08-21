17769a8422fd93ac1c6cfcd9e60f4caba5f8c30f
rathishubham7 <shubhamrathi@icloud.com>
2019-08-21 15:42:37 +0800

    changes

--- moonpay-api/API/index.js
+++ moonpay-api/API/index.js
@@ -3,38 +3,65 @@
  * @author Shubham rathi
  * @requires NPM:express,helmet,compression,cors,morgan
  */
-const express = require("express");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const compression = require("compression");
-const cors = require("cors");
-const morgan = require("morgan");
+const express = require('express')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const compression = require('compression')
+const cors = require('cors')
+const morgan = require('morgan')
+const log = require('loglevel')
+
+// setup loglevel 
+// Loglevel init
+const buildEnv = process.env.NODE_ENV
+let loglevel
+switch (buildEnv) {
+  case 'staging':
+    logLevel = 'info'
+    log.setDefaultLevel(logLevel)
+    break
+  case 'testing':
+    logLevel = 'debug'
+    log.setDefaultLevel(logLevel)
+    break
+  case 'development':
+    let logLevel = 'debug'
+    log.setDefaultLevel(logLevel)
+    break
+  default:
+    logLevel = 'error'
+    log.setDefaultLevel(logLevel)
+    log.disableAll()
+    break
+}
+log.info('TORUS_BUILD_ENV', process.env.TORUS_BUILD_ENV)
+
 
 // setup app
-const app = express();
+const app = express()
 
 // Setup environment
-require("dotenv").config();
+require('dotenv').config()
 
 // setup middleware
 const corsOptions = {
   //   origin: ["https://localhost:3000", /\.tor\.us$/],
-  origin: "*",
+  origin: '*',
   credentials: false
-};
+}
 
-if (process.env.NODE_ENV === "development") app.use(morgan("tiny")); // HTTP logging
-app.use(cors(corsOptions)); // middleware to enables cors
-app.use(helmet()); // middleware which adds http headers
-app.use(compression()); // middleware which uses gzip compression on responses
-app.use(bodyParser.urlencoded({ extended: false })); // middleware which parses body
-app.use(bodyParser.json()); // converts body to json
+if (process.env.NODE_ENV === 'development') app.use(morgan('tiny')) // HTTP logging
+app.use(cors(corsOptions)) // middleware to enables cors
+app.use(helmet()) // middleware which adds http headers
+app.use(compression()) // middleware which uses gzip compression on responses
+app.use(bodyParser.urlencoded({ extended: false })) // middleware which parses body
+app.use(bodyParser.json()) // converts body to json
 
 // bring all routes here
-const routes = require("./routes");
-app.use("/", routes);
+const routes = require('./routes')
+app.use('/', routes)
 
-const port = process.env.PORT || 2040;
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+const port = process.env.PORT || 2040
+app.listen(port, () => console.log(`Server running on port: ${port}`))
 
-module.exports = app; // For testing
+module.exports = app // For testing
