4b69ff46680e056f652cdfb9a0a389fb836b94aa
rathishubham7 <shubhamrathi@icloud.com>
2019-08-21 14:22:15 +0800

    APIs init

---
+++ moonpay-api/API/routes/index.js
@@ -0,0 +1,16 @@
+/**
+ * Parent file for express router
+ * @author Shubham Rathi
+ * @module ParentRouter
+ */
+
+/**
+ * express module
+ * @const
+ * @ignore
+ */
+const express = require("express");
+const router = express.Router();
+const defaultRoute = require("./default");
+router.use("/", defaultRoute);
+module.exports = router;
