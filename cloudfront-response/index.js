83b0f34e85a9ffe1eebca43e994c27b2dfc919bb
chaitanyapotti <chaitanya.potti@gmail.com>
2019-11-06 22:02:06 +0800

    add cloudfront response

---
+++ cloudfront-response/index.js
@@ -0,0 +1,52 @@
+"use strict";
+exports.handler = (event, context, callback) => {
+  const response = event.Records[0].cf.response;
+  const uri = event.Records[0].cf.request.uri;
+  const headers = response.headers;
+
+  if (uri.includes("service-worker.js")) {
+    headers["cache-control"] = [
+      {
+        key: "Cache-Control",
+        value: "max-age=3600"
+      }
+    ];
+
+    headers["service-worker-allowed"] = [
+      {
+        key: "Service-Worker-Allowed",
+        value: "/"
+      }
+    ];
+  }
+
+  headers["strict-transport-security"] = [
+    {
+      key: "Strict-Transport-Security",
+      value: "max-age=31536000; includeSubdomains; preload"
+    }
+  ];
+
+  headers["x-content-type-options"] = [
+    {
+      key: "X-Content-Type-Options",
+      value: "nosniff"
+    }
+  ];
+
+  headers["x-xss-protection"] = [
+    {
+      key: "X-XSS-Protection",
+      value: "1; mode=block"
+    }
+  ];
+
+  headers["referrer-policy"] = [
+    {
+      key: "Referrer-Policy",
+      value: "same-origin"
+    }
+  ];
+
+  callback(null, response);
+};
