54957c12fb269e2c99fc6b4b2420991930ecfff9
Archit <arch1995@gmail.com>
2023-04-03 16:48:41 +0530

    update regex for openlogin

--- cloudfront-openlogin-production-redirect/index.js
+++ cloudfront-openlogin-production-redirect/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const GLOBAL_VERSION = "v1.9.15";
+const GLOBAL_VERSION = "v3";
 
 exports.handler = (event, context, callback) => {
   const version = GLOBAL_VERSION;
@@ -11,19 +11,19 @@ exports.handler = (event, context, callback) => {
   // Extract the URI from the request
   const olduri = request.uri;
 
-  // if uri is like /v10/hello.js *(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG), then return same ^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\/..*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG)$
-  // if uri is like /v10/wallet non-extension files, then return app.tor.us/v10/index.html ^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\/[^\.]*$
-  // if uri is like /v10 or /v10/, then return app.tor.us/v10/index.html ^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*[\/]?$
-  // if uri is like /embed.min.js *(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG), then return app.tor.us/${version}/embed.min.js ^\/..*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG)$
-  // if uri is like /wallet or /wallet/ , then return app.tor.us/${version}/index.html ^\/[^\.]*$
-  // if uri is like /, then return app.tor.us/${version}/index.html app\.tor\.us\/$
+  // if uri is like /v10/hello.js *(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG), then return same ^\/v[0-9][0-9]*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG)$
+  // if uri is like /v10/wallet non-extension files, then return app.openlogin.com/v10/index.html ^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\/[^\.]*$
+  // if uri is like /v10 or /v10/, then return app.openlogin.com/v10/index.html ^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*[\/]?$
+  // if uri is like /embed.min.js *(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG), then return app.openlogin.com/${version}/embed.min.js ^\/..*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG)$
+  // if uri is like /wallet or /wallet/ , then return app.openlogin.com/${version}/index.html ^\/[^\.]*$
+  // if uri is like /, then return app.openlogin.com/${version}/index.html app\.openlogin\.com\/$
   let newuri;
-  if (RegExp(/^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\/..*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG|JSON|json|txt|gif)$/).test(olduri)) {
+  if (RegExp(/^\/v[0-9][0-9]*\/..*\.(js|css|png|PNG|svg|html|jpg|JPG|jpeg|JPEG|JSON|json|txt|gif)$/).test(olduri)) {
     newuri = olduri;
-  } else if (RegExp(/^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\/[^\.]*$/).test(olduri)) {
+  } else if (RegExp(/^\/v[0-9][0-9]*[^\.]*$/).test(olduri)) {
     const secondIndex = olduri.indexOf("/", 1);
     newuri = olduri.slice(0, secondIndex) + `/index.html`;
-  } else if (RegExp(/^\/v[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*[\/]?$/).test(olduri)) {
+  } else if (RegExp(/^\/v[0-9][0-9]*[\/]?$/).test(olduri)) {
     const secondIndex = olduri.indexOf("/", 1);
     const slicedOrignal = secondIndex === -1 ? olduri : olduri.slice(0, secondIndex);
     newuri = slicedOrignal + `/index.html`;
